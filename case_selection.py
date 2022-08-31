b0d9b51c6f922c468677ece800e925a29a339e06
[Salman-Noor1] <[salmannoormohammed769@gmail.com]>
2022-08-31 14:25:00 +0300

    game.python

---
+++ case_selection.py
@@ -0,0 +1,23 @@
+# Case Selection
+# Days Of The Week
+
+# Start
+
+days = input("Enter the number of the day ")
+# switchcase
+case = (1)
+print("Monday")
+case = (2)
+print("Tuesday")
+case = (3)
+print("Wednesday")
+case = (4)
+print("Thursday")
+case = (5)
+print("Friday")
+case = (6)
+print("Saturay") 
+case = ()
+print("Sunday")    
+
+# Stop
